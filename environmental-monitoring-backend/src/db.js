95b38a30f2d1926cb1d1261f5a65c2ea2bd7ef05
Luis Ignacio Rosales <103294514+RosalesLuis123@users.noreply.github.com>
2025-05-02 14:46:23 -0400

    Update db.js

--- environmental-monitoring-backend/src/db.js
+++ environmental-monitoring-backend/src/db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   user: 'postgres', // Cambia esto por tu usuario de PostgreSQL
   host: 'localhost',
   database: 'environmental_monitoring',
-  password: 'Payamon123', // Cambia esto por tu contraseña
+  password: 'password', // Cambia esto por tu contraseña
   port: 5433,
 })
 
